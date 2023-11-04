c5e8038de31126be97a2ecff9a475952cc5a37fb
GhostinToast <146139897+GhostinToast@users.noreply.github.com>
2023-11-04 14:10:48 -0400

    dictionary update, minor
    just changed from class to dictionary

--- pongServer.py
+++ pongServer.py
@@ -6,7 +6,7 @@
 # Misc:                     <Not Required.  Anything else you might want to include>
 # =================================================================================================
 
-#1.1- loose n rough, modifying nescesary(untested) (you proably shouldnt run this)
+#1.0.1- loose n rough, modifying nescesary(untested) (you proably shouldnt run this)
 import socket
 import threading
 #used to encode to binary for the sending to and from server
@@ -14,35 +14,30 @@ import json
 
 # Use this file to write your server logic
 # You will need to support at least two clients
-class gameSave:
+gameSave = {
     # You will need to keep track of where on the screen (x,y coordinates) each paddle is, the score 
     # for each player and where the ball is, and relay that to each client
-    leftlock = threading.Lock()
-    rightlock = threading.Lock()
+
     #bools that show if the left and right servers are on
-    lefton = False
-    righton = False
+    'lefton' : False,
+    'righton' : False,
 
-    rpadlock = threading.Lock()
+
     #x and y coordinates of the right paddle
-    rpad = [0,0]
-    lpadlock = threading.Lock()
-    #x and y coordinates of the left paddle
-    lpad = [0,0]
-    ballock = threading.Lock()
-    # x and y coordinates of the ball
-    ball = [0,0]
-    scorelock = threading.Lock()
-    #right and left hand side scores
-    score = [0,0]
+    'rpad': [0,0],
 
-    namelock = threading.Lock()
+    #x and y coordinates of the left paddle
+    'lpad' : [0,0],
+    # x and y coordinates of the ball
+    'ball':[0,0],
+    #right and left hand side scores
+    'score':[0,0],
     #player name, 1 or 2 (0 is the null)
-    name = 0
+    'name' : 0,
     # I suggest you use the sync variable in pongClient.py to determine how out of sync your two
     # clients are and take actions to resync the games
-    synclock = threading.Lock()
-    sync = 0
+    'sync' :0,
+}
 def client(socket, name):
     if name == 1: #left hand side stuff
         #check rhs sync
